5b5f224d68eba79a2d65392474375060343a063a
Davide Ginelli <ginellidavide@gmail.com>
2022-06-07 11:23:25 +0200

    First commit

---
+++ src/main/java/it/unimib/Author.java
@@ -0,0 +1,66 @@
+package it.unimib;
+
+public class Author {
+    private String name;
+    private String surname;
+    private int birthYear;
+
+    public Author(String name, String surname, int birthYear) {
+        this.name = name;
+        this.surname = surname;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (getBirthYear() != author.getBirthYear()) return false;
+        if (!getName().equals(author.getName())) return false;
+        return getSurname().equals(author.getSurname());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getSurname().hashCode();
+        result = 31 * result + getBirthYear();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
+}
